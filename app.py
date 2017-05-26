59037c8fc7368eede0bb3f206db6b10749d47b27
DaTTT111 <wz__@163.com>
2017-05-26 11:31:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-from flask import Flask                                                                                                 
-from redis import Redis                                                                                                 
-import os                                                                                                               
-                                                                                                                        
-app = Flask(__name__)                                                                                                   
-redis = Redis(host=os.getenv('REDIS_PORT_6379_TCP_ADDR', 'localhost'), port=int(os.getenv('REDIS_PORT_6379_TCP_PORT', 63
-79)))                                                                                                                   
-                                                                                                                        
-@app.route('/')                                                                                                         
-def hello():                                                                                                            
-    redis.incr('hits')                                                                                                  
-    return 'Hello alauda World! I have been seen %s times.' % redis.get('hits')                                                
-                                                                                                                        
-if __name__ == "__main__":                                                                                              
-        app.run(host="0.0.0.0", port=80, debug=True)  
+from flask import Flask
+from redis import Redis
+import os
+
+app = Flask(__name__)
+redis = Redis(host=os.getenv('REDIS_PORT_6379_TCP_ADDR', 'localhost'), port=int(os.getenv('REDIS_PORT_6379_TCP_PORT', 6379)))
+
+@app.route('/')
+def hello():
+    redis.incr('hits')
+    return 'Hello alauda World! I have been seen %s times.' % redis.get('hits')
+
+if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=80, debug=True)
